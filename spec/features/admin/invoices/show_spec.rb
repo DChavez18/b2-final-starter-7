f509f8833970188150a9b3a88ad75e854d4bd79b
Derek Chavez <frrenose17@yahoo.com>
2023-08-06 16:04:47 -0600

    refactor before each block to include bulk discounts

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -17,6 +17,11 @@ describe "Admin Invoices Index Page" do
     @ii_2 = InvoiceItem.create!(invoice_id: @i1.id, item_id: @item_2.id, quantity: 6, unit_price: 1, status: 1)
     @ii_3 = InvoiceItem.create!(invoice_id: @i2.id, item_id: @item_2.id, quantity: 87, unit_price: 12, status: 2)
 
+    @bulk_discount = BulkDiscount.create!(name: "Summer Sale", percentage_discount: 10, quantity_threshold: 10, merchant_id: @m1.id)
+
+    @item_1.bulk_discount = @bulk_discount
+    @item_1.save!
+    
     visit admin_invoice_path(@i1)
   end
 
@@ -83,6 +88,7 @@ describe "Admin Invoices Index Page" do
     end
     it "shows the total discounted revenue based on bulk discounts applied" do
       total_discounted_revenue = @i1.total_discounted_revenue
+      save_and_open_page
       expect(page).to have_content("Total Discounted Revenue: $#{total_discounted_revenue}")
     end
   end
